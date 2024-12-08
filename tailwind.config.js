f58cbf984ccafb2eb718a30479dff13fce46097a
Clemens Berkenhoff <clemens.berkenhoff@stud.htwk-leipzig.de>
2024-12-08 12:54:33 +0100

    init tailwindcss and cleaned up project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/style.css", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
