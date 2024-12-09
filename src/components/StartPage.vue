9d99fe94e1eca5f51d03897df8d8055c49f16280
Felix Schmeißer <felix.schmeisser@stud.htwk-leipzig.de>
2024-12-09 23:06:57 +0100

    add some example components

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+import Dashboard from './Dashboard.vue';
 
-const count = ref(0)
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1>{{ msg }}</h1>
-
-    <div>
-      <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="count++">count is {{ count }}</button>
-    </div>
-
-  </div>
+  <Dashboard/>
 </template>
-
-<style scoped>
-</style>
---
+++ src/components/ThemeSwitchButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Icon } from '@iconify/vue'
+import { Sun, Moon } from 'lucide-vue-next'
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode()
+
+const toggleMode = () => {
+  mode.value = mode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <Button variant="secondary" size="icon" class="rounded-full" @click="toggleMode">
+    <Sun class="h-5 w-5" v-if="mode === 'dark'" />
+    <Moon class="h-5 w-5" v-else />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+</template>
