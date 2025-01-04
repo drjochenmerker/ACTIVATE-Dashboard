6042596dffb557904b6ef3822666474cc7768fbe
Clemens Berkenhoff <clemens.berkenhoff@stud.htwk-leipzig.de>
2025-01-04 14:15:48 +0100

    add ActivityDiagram-Component to HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import NewItemDialog from '@/components/dialogs/NewItemDialog.vue';
+import ActivityDiagram from '@/components/ActivityDiagram.vue';
 </script>
 
 <template>
     <h1 class="text-2xl font-semibold mb-4">Home</h1>
 
     <div class="w-1/2 h-2/3 relative mx-auto">
-        <div
-            class="absolute inset-0 bg-muted animate-pulse"
-            style="clip-path: polygon(50% 0%, 0% 100%, 100% 100%)"
-        ></div>
+        <ActivityDiagram/>
     </div>
 
     <NewItemDialog />
