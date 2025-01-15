37cd0f1e2931343dcb01ac20149e13ac2479ec90
Kenn Kaubukowski <kenn.kaubukowski@htwk-leipzig.de>
2025-01-15 18:22:10 +0100

    Tried to add new test file, adapted sparql script, changed working examples according to the new naming scheme

--- scripts/sparql_setup.sh
+++ scripts/sparql_setup.sh
@@ -4,4 +4,7 @@ python -m venv ./activate_env
 source ./activate_env/bin/activate
 pip install --upgrade pip
 pip install rdflib-endpoint uvicorn
+# Should work as soon as the Syntax Errors in BeispielDatenCIRSFall.ttl have been resolved
+# FILES=$(echo ./src/assets/data/*.ttl) 
+# rdflib-endpoint serve $FILES
 rdflib-endpoint serve ./src/assets/data/Beispieldaten.ttl ./src/assets/data/vocabulary.ttl
