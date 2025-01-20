ac5d9b8ffc3161c02e78e003f2578b90d61d488d
Clemens Berkenhoff <clemens.berkenhoff@stud.htwk-leipzig.de>
2025-01-20 19:59:59 +0100

    refactor line structure

--- src/components/ActivityDiagram.vue
+++ src/components/ActivityDiagram.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted, watch } from "vue";
 import { useColorMode } from "@vueuse/core";
-import { getActivities, getActivityDetail } from "@/data/knowledge_graph/knowledge_graph";
 
 /** 
  * Activity-Diagram-Component
@@ -45,48 +44,24 @@ export default defineComponent({
 
         /**
          * Lines of the activity diagram
-         * @property {number} x1: x-coordinate of the stroke starting point
-         * @property {number} x2: x-coordinate of the stroke end point
-         * @property {number} y1: y-coordinate of the stroke starting point
-         * @property {number} y2: y-coordinate of the stroke end point
+         * @property {Array} pointIds: Array of point IDs that the line connects
          * @property {color} color: Color of the line
          * @property {bool} active: Specifies if the line is active at the moment
          */
-        const lines = [
-            { x1: triangleWidth / 2, y1: triangleHeight / 8, x2: triangleWidth / 8, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Instruments -> Subject
-            { x1: triangleWidth / 2, y1: triangleHeight / 8, x2: (triangleWidth / 8) * 7, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Instruments -> Object
-            { x1: triangleWidth / 8, y1: (triangleHeight / 8) * 7, x2: (triangleWidth / 8) * 7, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Subject -> Object
-            { x1: triangleWidth / 2, y1: triangleHeight / 8, x2: (triangleWidth / 16) * 5, y2: triangleHeight / 2, color: getLineColor(), active: false }, // Instruments -> Rules
-            { x1: triangleWidth / 2, y1: triangleHeight / 8, x2: (triangleWidth / 16) * 11, y2: triangleHeight / 2, color: getLineColor(), active: false }, // Instruments -> Division of Labour
-            { x1: triangleWidth / 8, y1: (triangleHeight / 8) * 7, x2: triangleWidth / 2, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Subject -> Community
-            { x1: (triangleWidth / 8) * 7, y1: (triangleHeight / 8) * 7, x2: triangleWidth / 2, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Object -> Community
-            { x1: triangleWidth / 8, y1: (triangleHeight / 8) * 7, x2: (triangleWidth / 16) * 5, y2: triangleHeight / 2, color: getLineColor(), active: false }, // Subject -> Rules
-            { x1: (triangleWidth / 8) * 7, y1: (triangleHeight / 8) * 7, x2: (triangleWidth / 16) * 11, y2: triangleHeight / 2, color: getLineColor(), active: false }, // Object -> Division of Labour
-            { x1: (triangleWidth / 16) * 5, y1: triangleHeight / 2, x2: (triangleWidth / 16) * 11, y2: triangleHeight / 2, color: getLineColor(), active: false }, // Rules -> Division of Labour
-            { x1: (triangleWidth / 16) * 5, y1: triangleHeight / 2, x2: triangleWidth / 2, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: false }, // Rules -> Community
-            { x1: (triangleWidth / 16) * 11, y1: triangleHeight / 2, x2: triangleWidth / 2, y2: (triangleHeight / 8) * 7, color: getLineColor(), active: true }, // Division of Labour -> Community
-        ];
-
-        // Refactoring-Attempt, maybe continue later
-        // const newLines = [
-        //     //{ pointIds: ["instruments", "rules"], color: getLineColor(), active: true },
-        //     //{ pointIds: ["instruments", "division_of_labour"], color: getLineColor(), active: false },
-        //     //{ pointIds: ["rules", "division_of_labour"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["instruments", "subject"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["instruments", "object"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["rules", "community"], color: getLineColor(), active: false },
-        //     { pointIds: ["community", "division_of_labour"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["subject", "rules"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["object", "division_of_labour"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["subject", "object"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["subject", "community"], color: getLineColor(), active: false }, 
-        //     { pointIds: ["community", "object"], color: getLineColor(), active: false }, 
-        // ]
-        //  const pointWithId = (id: string) => points.value.find(point => point.id === id);
+        const lines = ref([
+            { pointIds: ["instruments", "subject"], color: getLineColor(), active: false },
+            { pointIds: ["instruments", "object"], color: getLineColor(), active: false },
+            { pointIds: ["rules", "community"], color: getLineColor(), active: false },
+            { pointIds: ["community", "division_of_labour"], color: getLineColor(), active: false },
+            { pointIds: ["subject", "rules"], color: getLineColor(), active: false },
+            { pointIds: ["object", "division_of_labour"], color: getLineColor(), active: false },
+            { pointIds: ["subject", "object"], color: getLineColor(), active: false },
+            { pointIds: ["subject", "community"], color: getLineColor(), active: false },
+            { pointIds: ["community", "object"], color: getLineColor(), active: false },
+        ]);
 
         // Array of all points that are currently selected
         const selectedPoints = ref<number[]>([]);
-
 
         // applies point-colors based on current theme
         const updateColors = () => {
@@ -95,18 +70,12 @@ export default defineComponent({
                 point.color = point.selected === "true" ? "red" : getPointColor();
             });
 
-            // lines.forEach((line) => {
-            //     line.color = line.active === true ? "red" : getLineColor()
-            // })
-
             // Update line colors and active status
             if (selectedPoints.value.length === 2 || selectedPoints.value.length === 3) {
-                const selectedCoords = selectedPoints.value.map((i) => points.value[i]);
+                const selectedIds = selectedPoints.value.map((i) => points.value[i].id);
 
-                lines.forEach((line) => {
-                    const isConnecting =
-                        selectedCoords.some((p) => p.x === line.x1 && p.y === line.y1) &&
-                        selectedCoords.some((p) => p.x === line.x2 && p.y === line.y2);
+                lines.value.forEach((line) => {
+                    const isConnecting = line.pointIds.every((id) => selectedIds.includes(id));
 
                     if (isConnecting) {
                         line.color = "red";
@@ -118,7 +87,7 @@ export default defineComponent({
                 });
             } else {
                 // Reset all lines if selection is invalid
-                lines.forEach((line) => {
+                lines.value.forEach((line) => {
                     line.color = getLineColor();
                     line.active = false;
                 });
@@ -139,18 +108,20 @@ export default defineComponent({
             ctx.clearRect(0, 0, triangleWidth, triangleHeight);
 
             // draw lines
-            lines.forEach((line) => {
-                ctx.beginPath();
-                ctx.moveTo(line.x1, line.y1);
-                ctx.lineTo(line.x2, line.y2);
-                ctx.strokeStyle = line.color;
-                ctx.lineWidth = line.active ? 4 : 2;
-                ctx.stroke();
+            lines.value.forEach((line) => {
+                const [point1, point2] = line.pointIds.map((id) => points.value.find((p) => p.id === id));
+                if (point1 && point2) {
+                    ctx.beginPath();
+                    ctx.moveTo(point1.x, point1.y);
+                    ctx.lineTo(point2.x, point2.y);
+                    ctx.strokeStyle = line.color;
+                    ctx.lineWidth = line.active ? 4 : 2;
+                    ctx.stroke();
+                }
             });
 
             // draw circles where points are
             points.value.forEach((point) => {
-                //console.log(`Point Selected: ${point.selected} ; Point-Color: ${point.color}`)
                 ctx.beginPath();
                 ctx.arc(point.x, point.y, triangleHeight / 40, 0, 2 * Math.PI);
                 ctx.fillStyle = point.color;
@@ -181,47 +152,12 @@ export default defineComponent({
             }
         };
 
-        const triangles = [
-            [0, 3, 4],
-            [3, 1, 5],
-            [4, 2, 5],
-            [3, 4, 5],
-        ]
-
-        const isPointInTriangle = (px, py, ax, ay, bx, by, cx, cy) => {
-            const area = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) / 2;
-            const area1 = (px * (by - cy) + bx * (cy - py) + cx * (py - by)) / 2;
-            const area2 = (ax * (py - cy) + px * (cy - ay) + cx * (ay - py)) / 2;
-            const area3 = (ax * (by - py) + bx * (py - ay) + px * (ay - by)) / 2;
-
-            return Math.abs(area - (area1 + area2 + area3)) < 0.01;
-        };
-
-
-
-
         // Handles logic when a point is clicked
         const handleClick = (event: MouseEvent) => {
             if (!canvas.value) return;
             const rect = canvas.value.getBoundingClientRect();
             const mouseX = event.clientX - rect.left;
             const mouseY = event.clientY - rect.top;
-            console.log(mouseX, mouseY);
-
-            // for (const triangle of triangles) {
-            //     const [a, b, c] = triangle.map((i) => points.value[i]);
-            //     if (isPointInTriangle(mouseX, mouseY, a.x, a.y, b.x, b.y, c.x, c.y)) {
-            //         // Wähle alle Punkte dieses Dreiecks aus
-            //         triangle.forEach((index) => {
-            //             if (!selectedPoints.value.includes(index)) {
-            //                 selectedPoints.value.push(index);
-            //                 points.value[index].selected = "true";
-            //             }
-            //         });
-            //         updateColors();
-            //         return;
-            //     }
-            // }
 
             points.value.forEach((point, index) => {
                 const distance = Math.sqrt((mouseX - point.x) ** 2 + (mouseY - point.y) ** 2);
@@ -240,15 +176,12 @@ export default defineComponent({
         };
 
         onMounted(() => {
-            //console.log(getActivities());
-
             draw();
-
         });
 
         watch(mode, () => {
             updateColors();
-        })
+        });
 
         return {
             canvas,
