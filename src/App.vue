b7c8ce753c2078c9e6d5e076adfabcf2152c9203
Kenn Kaubukowski <kenn.kaubukowski@htwk-leipzig.de>
2025-02-27 15:38:05 +0100

    Fixed example in Main

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 import NavBar from './components/NavBar.vue';
 // Backend Tests
 import { getPredicateObject } from './data/knowledge_graph/read_operations';
-import { KnowledeGraphActivityClass, LanguageCode } from './data/knowledge_graph/structures';
+import { KnowledgeGraphActivityClass, LanguageCode } from './data/knowledge_graph/structures';
 import { vocabAddPredicate } from './data/knowledge_graph/write_operations';
 getPredicateObject().then(async preds => {
     console.log("Getting all predicates for Subject -> Object", preds.get(["subject","object"]))
     console.log("Bidirectional predicate get for Subject <-> Object", preds.getBidirectional(["subject","object"]))
     console.log("Adding predicates", await vocabAddPredicate("BorrowsFrom", 
-        [KnowledeGraphActivityClass.subject], [KnowledeGraphActivityClass.object, KnowledeGraphActivityClass.community], 
+        [KnowledgeGraphActivityClass.subject], [KnowledgeGraphActivityClass.object, KnowledgeGraphActivityClass.community], 
         [{label: "Borgt von", language: LanguageCode.german},{label: "Borrows from", language: LanguageCode.english}]
     ));
 })
