045eb6674bc18efc8d93b6305e53cfc808f25c04
Felix Schmeißer <felix.schmeisser@stud.htwk-leipzig.de>
2025-06-15 10:17:57 +0200

    vue-mess-detector measures

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "eslint/config";
+import pluginVue from "eslint-plugin-vue";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig([
+  {
+    files: ["**/*.ts", "**/*.vue"],
+    parser: tsParser,
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+      extraFileExtensions: [".vue"],
+      sourceType: "module",
+    },
+    plugins: {
+      vue: pluginVue,
+      "@typescript-eslint": tsPlugin,
+    },
+    extends: [
+      // NICHT als String, sondern aus dem Plugin-Objekt:
+      ...pluginVue.configs.essential.extends || [],
+      ...tsPlugin.configs.recommended.extends || [],
+    ],
+    rules: {
+      "@typescript-eslint/complexity": ["warn", { max: 10 }],
+      "vue/max-depth": ["warn", { max: 5 }],
+      "no-unused-vars": "off",
+    },
+  },
+]);
---
+++ src/components/complexity-tests/four.vue
@@ -0,0 +1,9 @@
+<template>
+  <p v-show="foo && bar">Hello</p>
+</template>
+
+<script>
+export default {
+  props: ['foo', 'bar']
+}
+</script>
