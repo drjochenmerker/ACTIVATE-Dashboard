cd34ccf7fcabda7fe74bb6c7cd5f0a9b182a19f9
alexanderreiprich <al3xz0n3@gmail.com>
2026-01-12 15:42:49 +0100

    added translations

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -62,16 +62,16 @@ const login = async () => {
 
       <div class="flex justify-center my-6">
 				<form>
-					<input v-model="passwordInput" type="password" class="dark:bg-gray-900" :placeholder="staticContent.placeholders.password[sessionStore.activeLanguage]" }}>
+					<input v-model="passwordInput" type="password" class="dark:bg-gray-900" :placeholder="staticContent.placeholders.password[sessionStore.activeLanguage]">
 				</form>
       </div>
 			<div v-if="showValidationErrors" class="flex justify-center my-6">
 				<p class="mt-4 text-red-500">{{ staticContent.errors.incorrectPassword[sessionStore.activeLanguage] }}</p>
 			</div>
 			<div class="flex justify-center my-6">
-				<Button @click="login" class="text-l px-6 py-3 rounded-full text-black bg-white border border-black hover:bg-black hover:text-white transition-colors duration-300">
+				<button @click="login" class="text-l px-6 py-3 rounded-full text-black bg-white border border-black hover:bg-black hover:text-white transition-colors duration-300">
           {{ staticContent.login.loginButton[sessionStore.activeLanguage] }}
-        </Button>
+        </button>
 			</div>
     </Card>
   </div>
